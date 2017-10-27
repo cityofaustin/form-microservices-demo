28c23f9462ed4ab5c959c104b835fab7fdd66904
Matt Langan <matt.langan@austintexas.gov>
2017-10-27 15:39:49 -0500

    Create initial AJAX-based form submission and response handling

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,24 @@
 class HomeController < ApplicationController
   def index
   end
+  
+  def github_form
+    render_environments
+  end
+  
+  def email_form
+    render_environments
+  end
+  
+  private
+    def render_environments
+      if Rails.env.development?
+        @destination_url = "http://localhost:3000"
+        @destination_name = "localhost"
+      elsif Rails.env.production?
+        @destination_url = "https://coa-test-form-api.herokuapp.com"
+        @destination_name = "heroku"
+      end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   root 'home#index'
+  match 'email_form' => 'home#email_form', via: [:get]
+  match 'github_form' => 'home#github_form', via: [:get]
 end
